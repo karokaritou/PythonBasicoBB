52507574b080da9529ec7c89188edf979c68c72f
karokaritou <gomezkaromy@gmail.com>
2026-02-12 19:03:41 -0800

    Agregamos otra lista de diccionario

--- sesion_02.py
+++ sesion_02.py
@@ -4,6 +4,17 @@ print ("Mi tupla: ", mi_tupla)
 
 #Lista
 mi_lista = [1, 3.1416, "Karo", mi_tupla]
-print("El primer elemento de la lista: ", mi_lista [0])
-print("El cuarto elemento de la lista: ", mi_lista [3])
-print("El tercer elemento de la lista: ", mi_lista [2])
+print("El primer elemento de la lista: ", mi_lista[0])
+print("El cuarto elemento de la lista: ", mi_lista[3])
+print("El tercer elemento de la lista: ", mi_lista[2])
+
+#Diccionarios
+mi_diccionario = {
+    "mi_lista": mi_lista,
+    "nombre": "Karo",
+    "Pi": 3.1416,
+    "Tel": "6648525089"
+}
+print("Llave para accesar a mi diccionario mi lista", mi_diccionario["mi_lista"])
+print("Llave para accesar a mi diccionario Pi",mi_diccionario["Pi"])
+print("Llave para accesar a mi diccionario Tel", mi_diccionario["Tel"])

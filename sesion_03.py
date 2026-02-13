a186eb79f8db4ea651a86a6187745f483f4bc08c
karokaritou <gomezkaromy@gmail.com>
2026-02-12 19:30:29 -0800

    Hice una lista con lo dias de la semana

--- sesion_03.py
+++ sesion_03.py
@@ -10,5 +10,10 @@ for i in mi_lista:
 
         print (f"El resultado de la suma de mi lista es: {resultado} ")
 
-        for i in range (2,9):
-            print(i)
+for i in range (2,9):
+    print(i)
+
+mi_lista_2 = ["lunes", "martes","miercoles","jueves","viernes",]
+for i in mi_lista_2:
+    if i != "lunes":
+       print(f"Feliz {i}!")

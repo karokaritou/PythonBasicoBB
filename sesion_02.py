b5c106fc00ecf9162f6b329d21a344765bede081
karokaritou <gomezkaromy@gmail.com>
2026-02-12 18:40:56 -0800

    Hice otro documento con una lista

---
+++ sesion_02.py
@@ -0,0 +1,9 @@
+#Tuplas
+mi_tupla = (2,4)
+print ("Mi tupla: ", mi_tupla)
+
+#Lista
+mi_lista = [1, 3.1416, "Karo", mi_tupla]
+print("El primer elemento de la lista: ", mi_lista [0])
+print("El cuarto elemento de la lista: ", mi_lista [3])
+print("El tercer elemento de la lista: ", mi_lista [2])
